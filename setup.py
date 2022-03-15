c6ce80c85342c93f1cb859a56342d22f08220efd
Liam Berti <liamtberti@gmail.com>
2022-03-15 17:36:59 +0000

    initial commit with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Demonstration of object detetection using pytorch',
+    author='Liam Berti',
+    license='MIT',
+)
